18b786b543bc44998c27aac421e939898ed785c4
anhthotn <22521424@gm.uit.edu.vn>
2025-07-18 13:52:32 +0700

    Django Tutorial Part 7: Sessions Framework

--- catalog/views.py
+++ catalog/views.py
@@ -14,15 +14,19 @@ def index(request):
     num_instances = BookInstance.objects.count()
     num_instances_available = BookInstance.objects.filter(status=LOAN_STATUS_AVAILABLE).count()
     num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits + 1
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits, 
     }
 
     return render(request, 'index.html', context=context)
+
 
 
 class BookListView(generic.ListView):
