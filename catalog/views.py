930c17468b7435883af443203348bc38c2cf38a1
thotna-0700 <126951867+thotna-0700@users.noreply.github.com>
2025-07-18 13:28:33 +0700

    Django Tutorial Part 6: Generic list and detail views (#5)

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
 from catalog.models import Book, Author, BookInstance
-from catalog.constants import LOAN_STATUS_AVAILABLE
+from catalog.constants import (
+    LOAN_STATUS_AVAILABLE,
+    BOOK_PAGINATE_BY,
+    LOAN_STATUS_ON_LOAN
+)
 
 
 def index(request):
@@ -18,3 +23,23 @@ def index(request):
     }
 
     return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = BOOK_PAGINATE_BY
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = "catalog/book_detail.html"
+    context_object_name = "book"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["book_instances"] = BookInstance.objects.filter(book=self.object)
+        context["book_genres"] = self.object.genre.all()  
+        context["AVAILABLE"] = LOAN_STATUS_AVAILABLE
+        context["ON_LOAN"] = LOAN_STATUS_ON_LOAN
+        context["MAINTENANCE"] = "m"
+        return context
