0e4986c253d602764579b9ac569c51d5e1c6d8bb
anhthotn <22521424@gm.uit.edu.vn>
2025-07-21 14:14:17 +0700

    Django Tutorial Part 9: Working with forms

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,4 @@
+import datetime
 from django.shortcuts import render
 from django.views import generic
 from catalog.models import Book, Author, BookInstance
@@ -6,8 +7,15 @@ from catalog.constants import (
     BOOK_PAGINATE_BY,
     LOAN_STATUS_ON_LOAN
 )
+from catalog.constants import DEFAULT_AUTHOR_DEATH_DATE
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.auth.decorators import permission_required, login_required
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from catalog.forms import RenewBookForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 
 def index(request):
@@ -24,11 +32,10 @@ def index(request):
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
-        'num_visits': num_visits, 
+        'num_visits': num_visits,
     }
 
     return render(request, 'index.html', context=context)
-
 
 
 class BookListView(generic.ListView):
@@ -44,7 +51,7 @@ class BookDetailView(generic.DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["book_instances"] = BookInstance.objects.filter(book=self.object)
-        context["book_genres"] = self.object.genre.all()  
+        context["book_genres"] = self.object.genre.all()
         context["AVAILABLE"] = LOAN_STATUS_AVAILABLE
         context["ON_LOAN"] = LOAN_STATUS_ON_LOAN
         context["MAINTENANCE"] = "m"
@@ -64,16 +71,15 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['loan_status_on_loan'] = LOAN_STATUS_ON_LOAN  
+        context['loan_status_on_loan'] = LOAN_STATUS_ON_LOAN
         return context
 
 
-@login_required  
-@permission_required('catalog.can_mark_returned', raise_exception=True)  
+@login_required
+@permission_required('catalog.can_mark_returned', raise_exception=True)
 def mark_book_returned(request, bookinstance_id):
     """View để đánh dấu sách là đã trả."""
     from django.shortcuts import get_object_or_404, redirect
-    from django.utils import timezone
 
     book_instance = get_object_or_404(BookInstance, pk=bookinstance_id)
     book_instance.status = LOAN_STATUS_AVAILABLE
@@ -81,4 +87,49 @@ def mark_book_returned(request, bookinstance_id):
     book_instance.borrower = None
     book_instance.save()
 
-    return redirect('all-borrowed')  
+    return redirect('all-borrowed')
+
+
+@login_required
+@permission_required('catalog.can_mark_returned', raise_exception=True)
+def renew_book_librarian(request, pk):
+    """View function for renewing a specific BookInstance by librarian."""
+    book_instance = get_object_or_404(BookInstance, pk=pk)
+
+    if request.method == 'POST':
+        form = RenewBookForm(request.POST)
+
+        if form.is_valid():
+            book_instance.due_back = form.cleaned_data['renewal_date']
+            book_instance.save()
+            return HttpResponseRedirect(reverse('all-borrowed'))
+
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
+
+    context = {
+        'form': form,
+        'book_instance': book_instance,
+    }
+
+    return render(request, 'catalog/book_renew_librarian.html', context)
+
+
+class AuthorCreate(PermissionRequiredMixin, CreateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    initial = {'date_of_death': DEFAULT_AUTHOR_DEATH_DATE}
+    permission_required = 'catalog.add_author'
+
+
+class AuthorUpdate(PermissionRequiredMixin, UpdateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    permission_required = 'catalog.change_author'
+
+
+class AuthorDelete(PermissionRequiredMixin, DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
+    permission_required = 'catalog.delete_author'
